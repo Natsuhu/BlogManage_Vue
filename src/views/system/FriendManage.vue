350c20820a6edd2de962b403b15e30e663015232
xiongjianqi <1127695505Jq>
2023-12-19 17:44:00 +0800

    完成友链管理

--- src/views/system/FriendManage.vue
+++ src/views/system/FriendManage.vue
@@ -36,7 +36,7 @@
             友链管理
           </div>
           <div style="width: 50%; display: flex; justify-content: flex-end">
-            <el-button size="medium" type="primary">新增</el-button>
+            <el-button @click="addFriend" size="medium" type="primary">新增</el-button>
             <el-button @click="getTableData(false)" size="medium" plain type="info"
                        class="base_refresh_btn base_margin_l_small"
                        icon="el-icon-refresh-right"/>
@@ -59,20 +59,23 @@
             <el-table-column label="点击数" prop="click" align="center" width="120"/>
             <el-table-column label="公开" prop="isPublished" align="center" width="120">
               <template slot-scope="scope">
-                <el-switch v-model="scope.row.isPublished" @change="updateAnnexAtt(scope.row)"/>
+                <el-switch v-model="scope.row.isPublished" @change="updateFriendAtt(scope.row)"/>
               </template>
             </el-table-column>
             <el-table-column label="创建时间" prop="createTime" align="center" width="160"/>
             <el-table-column label="更新时间" prop="updateTime" align="center" width="160"/>
 
             <!-- 操作按钮 -->
-            <el-table-column label="操作" width="120" align="center">
+            <el-table-column label="操作" width="200" align="center">
               <template slot-scope="scope">
+                <el-tooltip effect="dark" content="访问" placement="top">
+                  <a :href="scope.row.website" target="_blank" class="el-icon-view base_text_point"></a>
+                </el-tooltip>
                 <el-tooltip effect="dark" content="编辑" placement="top">
-                  <i class="el-icon-edit-outline base_text_point base_margin_r" @click="changeAnnex(scope.row)"/>
+                  <i class="el-icon-edit-outline base_text_point base_margin_lr" @click="changeFriend(scope.row)"/>
                 </el-tooltip>
                 <el-popconfirm confirm-button-text='好' cancel-button-text='手滑了' icon="el-icon-info" icon-color="red"
-                               title="这可是物理删除！" @onConfirm="removeAnnex(scope.row)">
+                               title="这可是物理删除！" @onConfirm="removeFriend(scope.row)">
                   <i slot="reference" class="el-icon-delete base_text_point"/>
                 </el-popconfirm>
               </template>
@@ -92,7 +95,7 @@
     </el-row>
 
     <!--  友链页面内容编辑区  -->
-    <el-form :model="form" :rules="formRules" ref="formRef" label-position="top">
+    <el-form label-position="top">
       <el-row>
         <el-container>
           <el-header style="border-bottom: 2px solid rgb(241, 242, 243);">
@@ -119,6 +122,45 @@
         </el-container>
       </el-row>
     </el-form>
+
+    <!-- 新增/修改对话框 -->
+    <el-dialog :title="dialogTitle" :visible.sync="dialog" :lock-scroll="false">
+
+      <!-- 名称输入表单 -->
+      <el-form label-position="top" label-width="80px">
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <el-form-item label="昵称">
+              <el-input v-model="form.nickname" prefix-icon="el-icon-user" placeholder="输入昵称"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="头像URL">
+              <el-input v-model="form.avatar" prefix-icon="el-icon-camera" placeholder="输入头像URL"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <el-form-item label="一句话">
+              <el-input v-model="form.description" prefix-icon="el-icon-chat-dot-square" placeholder="输入个签"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="网站">
+              <el-input v-model="form.website" prefix-icon="el-icon-bank-card" placeholder="输入个人主页"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+
+      <!-- 操作按钮 -->
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialog = false">取 消</el-button>
+        <el-button @click="submit" type="primary" >确 定</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -151,6 +193,30 @@ export default {
       friendPageSetting: {
         friendIsComment: false,
         friendContent: null
+      },
+      saveFlag: null,
+      dialogTitle: null,
+      dialog: false,
+      form: {
+        id: null,
+        nickname: null,
+        description: null,
+        avatar: null,
+        website: null,
+      }
+    }
+  },
+
+  watch: {
+    //监听对话框关闭
+    dialog: function (value) {
+      if (!value) {
+        this.saveFlag = 0;
+        this.form.id = null;
+        this.form.nickname = null;
+        this.form.description = null;
+        this.form.avatar = null;
+        this.form.website = null;
       }
     }
   },
@@ -221,6 +287,97 @@ export default {
     handleCurrentChange(newPage) {
       this.queryParam.pageNo = newPage
       this.getTableData()
+    },
+    addFriend() {
+      this.dialogTitle = "新增友链";
+      this.saveFlag = 1;
+      this.dialog = true;
+      this.form.id = null;
+      this.form.nickname = null;
+      this.form.description = null;
+      this.form.avatar = null;
+      this.form.website = null;
+    },
+    changeFriend(row) {
+      this.dialogTitle = "更新友链";
+      this.saveFlag = 2;
+      this.dialog = true;
+      this.form.id = row.id;
+      this.form.nickname = row.nickname;
+      this.form.description = row.description;
+      this.form.avatar = row.avatar;
+      this.form.website = row.website;
+    },
+    submit() {
+      if (this.saveFlag === 1) {
+        saveFriend(this.form).then(res => {
+          if (res.success) {
+            Notification({
+              title: '新增成功',
+              type: 'success',
+              duration: 1500
+            })
+            //刷新表单,关闭对话框
+            this.getTableData();
+            this.dialog = false;
+          } else {
+            Notification({
+              title: '新增失败',
+              message: res.msg,
+              type: 'error'
+            })
+          }
+        })
+      }
+      if (this.saveFlag === 2) {
+        this.updateFriendAtt(false);
+      }
+    },
+    updateFriendAtt(isRow) {
+      let data = {};
+      if (isRow) {
+        data.id = isRow.id;
+        data.isPublished = isRow.isPublished;
+      } else {
+        data = this.form
+      }
+      updateFriend(data).then(res => {
+        if (res.success) {
+          Notification({
+            title: '更新成功',
+            type: 'success',
+            duration: 1500
+          })
+          //刷新表单,关闭对话框
+          this.getTableData();
+          this.dialog = false;
+        } else {
+          Notification({
+            title: '更新失败',
+            message: res.msg,
+            type: 'error'
+          })
+        }
+      })
+    },
+    removeFriend(row) {
+      deleteFriend(row).then(res => {
+        if (res.success) {
+          Notification({
+            title: '删除成功',
+            type: 'success',
+            duration: 1500
+          })
+          //删除成功刷新表格
+          this.getTableData();
+        } else {
+          Notification({
+            title: '删除失败',
+            message: res.msg,
+            type: 'error'
+          })
+        }
+      })
     }
   }
 }
@@ -236,14 +393,14 @@ export default {
   align-items: center;
 }
 
-.el-icon-edit-outline {
+.el-icon-edit-outline, .el-icon-view {
   font-size: 1.2rem;
   font-weight: 100;
   color: #606266;
   transition: color .15s linear;
 }
 
-.el-icon-edit-outline:hover {
+.el-icon-edit-outline:hover, .el-icon-view:hover {
   color: #66ccff;
 }
 
