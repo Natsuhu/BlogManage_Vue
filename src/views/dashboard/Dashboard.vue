c950d5b805aa79b5e12aacbd5502cfe3461b8af3
NatsuKaze <1127695505@qq.com>
2025-02-07 11:34:28 +0800

    完成访客地图

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -51,17 +51,17 @@
     <el-row class="base_margin_b" :gutter="20">
       <el-col :span="8">
         <el-card shadow="never">
+          <div ref="mapEcharts" style="height:500px;"/>
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card shadow="never">
           <div ref="categoryEcharts" style="height:500px;"/>
         </el-card>
       </el-col>
       <el-col :span="8">
         <el-card shadow="never">
           <div ref="tagEcharts" style="height:500px;"/>
-        </el-card>
-      </el-col>
-      <el-col :span="8">
-        <el-card shadow="never">
-          <div ref="mapEcharts" style="height:500px;">访客地图（开发中...）</div>
         </el-card>
       </el-col>
     </el-row>
@@ -76,6 +76,8 @@
 
 <script>
 import echarts from "echarts";
+import 'echarts/map/js/china';
+import geoCoordMap from '@/utils/city2coord.json';
 import {getTopBaseData, getCategoryEcharts, getTagEcharts, getMapEcharts, getVisitRecordEcharts} from "@/api/Dashboard";
 export default {
   name: "Dashboard",
@@ -155,6 +157,153 @@ export default {
           }
         ]
       },
+      //地图
+      mapOption: {
+        title: {
+          text: '访客地图',
+          x: 'center'
+        },
+        tooltip: {
+          show: false
+        },
+        geo: {
+          map: "china",
+          roam: false,//关闭拖拽
+          zoom: 1.24,
+          center: [104.2, 36],//调整地图位置
+          label: {
+            normal: {
+              show: false,//关闭省份名展示
+              fontSize: "10",
+              color: "rgba(0,0,0,0.7)"
+            },
+            emphasis: {
+              show: false
+            }
+          }
+          // ,
+          // //注掉itemStyle块以去掉地图阴影
+          // itemStyle: {
+          //   normal: {
+          //     areaColor: "#0d0059",
+          //     borderColor: "#389dff",
+          //     borderWidth: 1,//设置外层边框
+          //     shadowBlur: 5,
+          //     shadowOffsetY: 8,
+          //     shadowOffsetX: 0,
+          //     shadowColor: "#01012a"
+          //   },
+          //   emphasis: {
+          //     areaColor: "#184cff",
+          //     shadowOffsetX: 0,
+          //     shadowOffsetY: 0,
+          //     shadowBlur: 5,
+          //     borderWidth: 0,
+          //     shadowColor: "rgba(0, 0, 0, 0.5)"
+          //   }
+          // }
+        },
+        series: [
+          {
+            type: "map",
+            map: "china",
+            roam: false,
+            zoom: 1.24,
+            center: [104.2, 36],
+            showLegendSymbol: false,
+            label: {
+              normal: {
+                show: false
+              },
+              emphasis: {
+                show: false
+              }
+            },
+            itemStyle: {
+              normal: {
+                areaColor: "#eae0bd",
+                borderColor: "#6f641f",
+                borderWidth: 0.5
+              },
+              emphasis: {
+                areaColor: "#8ed1eb",
+                shadowOffsetX: 0,
+                shadowOffsetY: 0,
+                shadowBlur: 5,
+                borderWidth: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            }
+          },
+          {
+            name: "",
+            type: "scatter",
+            coordinateSystem: "geo",
+            data: [],
+            symbol: "circle",
+            symbolSize: 6,
+            hoverSymbolSize: 10,
+            tooltip: {
+              formatter(value) {
+                return value.data.name + "<br/>" + "访客数：" + value.data.uv
+              },
+              show: true
+            },
+            encode: {
+              value: 2
+            },
+            label: {
+              formatter: "{b}",
+              position: "right",
+              show: false
+            },
+            itemStyle: {
+              color: "#c97d0b"
+            },
+            emphasis: {
+              label: {
+                show: false
+              }
+            }
+          },
+          {
+            name: "Top 5",
+            type: "effectScatter",
+            coordinateSystem: "geo",
+            data: [],
+            symbol: "circle",
+            symbolSize: 6,
+            tooltip: {
+              formatter(value) {
+                return value.data.name + "<br/>" + "访客数：" + value.data.uv
+              },
+              show: true
+            },
+            encode: {
+              value: 2
+            },
+            showEffectOn: "render",
+            rippleEffect: {
+              brushType: "stroke",
+              color: "#c97d0b",
+              period: 9,
+              scale: 5
+            },
+            hoverAnimation: true,
+            label: {
+              formatter: "{b}",
+              position: "right",
+              show: true
+            },
+            itemStyle: {
+              color: "#c97d0b",
+              shadowBlur: 1,
+              shadowColor: "#333"
+            },
+            zlevel: 1
+          }
+        ]
+      },
       //访问量折线图
       visitRecordOption: {
         title: {
@@ -203,7 +352,8 @@ export default {
     this.getTopData();
     this.getCategoryData();
     this.getTagData();
-    this.initVisitRecordEcharts();
+    this.getMapData();
+    this.getLineData();
   },
   methods: {
     getTopData() {
@@ -226,6 +376,18 @@ export default {
         this.initTagEcharts()
       })
     },
+    getMapData() {
+      getMapEcharts().then(res => {
+        let mapData = this.convertData(res.data)
+        this.mapOption.series[1].data = mapData
+        this.mapOption.series[2].data = mapData.splice(0, 5)
+        this.initMapEcharts();
+      })
+    },
+    getLineData() {
+      //Mock
+      this.initVisitRecordEcharts()
+    },
     initCategoryEcharts() {
       this.categoryEcharts = echarts.init(this.$refs.categoryEcharts, 'light')
       this.categoryEcharts.setOption(this.categoryOption)
@@ -241,6 +403,20 @@ export default {
     initVisitRecordEcharts() {
       this.visitRecordEcharts = echarts.init(this.$refs.visitRecordEcharts)
       this.visitRecordEcharts.setOption(this.visitRecordOption)
+    },
+    convertData(data) {
+      let res = []
+      for (let i = 0; i < data.length; i++) {
+        let geoData = geoCoordMap[data[i].name]
+        if (geoData) {
+          res.push({
+            name: data[i].name,
+            value: geoData,
+            uv: data[i].value
+          })
+        }
+      }
+      return res
     }
   }
 }
